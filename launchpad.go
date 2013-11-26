48062cc5b4e3e044296ee806840b3275e2066b55
Burcu Dogan <burcud@google.com>
2013-11-26 11:41:41 +0100

    Adding Launchpad in/out midi device discovery and introducing input related utilities.

--- launchpad.go
+++ launchpad.go
@@ -15,31 +15,77 @@
 package launchpad
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/rakyll/portmidi"
 )
 
 type Launchpad struct {
-	output *portmidi.Stream
+	inputStream  *portmidi.Stream
+	outputStream *portmidi.Stream
 }
 
-func New(deviceId portmidi.DeviceId) (pad *Launchpad, err error) {
-	var stream *portmidi.Stream
-	if stream, err = portmidi.NewOutputStream(deviceId, 1024, 0); err != nil {
+func New() (*Launchpad, error) {
+	input, output, err := discover()
+	if err != nil {
 		return nil, err
 	}
-	return &Launchpad{output: stream}, nil
+
+	var inStream, outStream *portmidi.Stream
+	if inStream, err = portmidi.NewInputStream(input, 1024); err != nil {
+		return nil, err
+	}
+	if outStream, err = portmidi.NewOutputStream(output, 1024, 0); err != nil {
+		return nil, err
+	}
+	return &Launchpad{inputStream: inStream, outputStream: outStream}, nil
+}
+
+func (l *Launchpad) Listen() {
+	panic("not implemented")
+}
+
+func (l *Launchpad) Read() (events []*portmidi.Event, err error) {
+	return l.inputStream.Read(64)
 }
 
 func (l *Launchpad) Light(x, y, g, r int) error {
 	note := int64(x + 16*(7-y))
 	velocity := int64(16*g + r + 8 + 4)
-	return l.output.WriteShort(0x90, note, velocity)
+	return l.outputStream.WriteShort(0x90, note, velocity)
 }
 
 func (l *Launchpad) Reset() error {
-	return l.output.WriteShort(0xb0, 0, 0)
+	return l.outputStream.WriteShort(0xb0, 0, 0)
 }
 
 func (l *Launchpad) Cleanup() error {
-	return l.output.Close()
+	if err := l.inputStream.Close(); err != nil {
+		return err
+	}
+	return l.outputStream.Close()
+}
+
+func discover() (input portmidi.DeviceId, output portmidi.DeviceId, err error) {
+	in := -1
+	out := -1
+	for i := 0; i < portmidi.CountDevices(); i++ {
+		info := portmidi.GetDeviceInfo(portmidi.DeviceId(i))
+		if strings.Contains(info.Name, "Launchpad") {
+			if info.IsInputAvailable {
+				in = i
+			}
+			if info.IsOutputAvailable {
+				out = i
+			}
+		}
+	}
+	if in == -1 || out == -1 {
+		err = errors.New("portmidi: No Launchpad is connected.")
+	} else {
+		input = portmidi.DeviceId(in)
+		output = portmidi.DeviceId(out)
+	}
+	return
 }
