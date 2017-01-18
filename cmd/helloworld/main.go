d535173fb7fcd7192fa0bcf2404c73585d850a70
Jaana Burcu Dogan <jbd@google.com>
2017-01-18 15:36:09 -0800

    Simplify the example.

---
+++ cmd/helloworld/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/rakyll/launchpad"
+)
+
+func main() {
+	pad, err := launchpad.Open()
+	if err != nil {
+		log.Fatalf("error while openning connection to launchpad: %v", err)
+	}
+	pad.Reset()
+
+	// Set <0,0> to yellow.
+	pad.Light(0, 0, 2, 2)
+
+	ch := pad.Listen()
+	for {
+		hit := <-ch
+		log.Printf("Button pressed at <x=%d, y=%d>", hit.X, hit.Y)
+		// Turn to green.
+		pad.Light(hit.X, hit.Y, 2, 0)
+	}
+}
