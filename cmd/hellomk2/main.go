b2437b463d1830df1cd44e2a1e97ed68694a15ae
Jaana Burcu Dogan <jbd@google.com>
2017-03-19 22:48:32 -0700

    cmd/hellomk2: render the whole color palette

--- cmd/hellomk2/main.go
+++ cmd/hellomk2/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/rakyll/launchpad/mk2"
 )
@@ -13,17 +14,26 @@ func main() {
 	}
 	defer pad.Close()
 
-	// Turn all buttons to bright red.
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
-			pad.Light(i, j, 72)
+	var color int
+	render := func(i, j int) {
+		pad.Reset()
+		// Turn all buttons to bright red.
+		for i := 0; i < 8; i++ {
+			for j := 0; j < 8; j++ {
+				pad.Light(i, j, color)
+				time.Sleep(20 * time.Millisecond)
+				color = (color + 1) % 128
+			}
 		}
 	}
 
+	render(0, 0)
 	ch := pad.Listen()
 	for {
 		hit := <-ch
 		log.Printf("Button pressed at <x=%d, y=%d>", hit.X, hit.Y)
-		// Turn to green.
+		// Re-render the color palette again.
+		render(hit.X, hit.Y)
 	}
+
 }
